62d595b8b5587f3893512d4aea9cc61b2a8123bd
ochelott <benjash8@outlook.com>
2026-05-04 01:45:20 -0300

    Ejercicio 1 Condicion del Alumno

---
+++ Ejercicio 1 Condicion del Alumno/Condicion del Alumno/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicion_del_Alumno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3, promedio;
+            Console.WriteLine("Ingrese las notas de los 3 parciales: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            n2 = Convert.ToDouble(Console.ReadLine());
+            n3 = Convert.ToDouble(Console.ReadLine());
+            promedio = (n1 + n2 + n3) / 3;
+            if (promedio > 7)
+            {
+                Console.WriteLine("Promociona");
+            }
+            else
+            {
+                Console.WriteLine("Rinde Final");
+            }
+            Console.ReadKey();
+        }
+            }
+        }
