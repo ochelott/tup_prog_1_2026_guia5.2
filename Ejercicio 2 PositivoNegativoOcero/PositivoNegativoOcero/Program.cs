0a79b123bcb2c33ddcc0fa9e6025508e4f45f777
ochelott <benjash8@outlook.com>
2026-05-04 02:01:47 -0300

    Ejercicio 2 PositivoNegativoOcero

---
+++ Ejercicio 2 PositivoNegativoOcero/PositivoNegativoOcero/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositivoNegativoOcero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Ingrese un número:");
+                num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0)
+            {
+                Console.WriteLine("El número es positivo.");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("El número es negativo.");
+            }
+            else
+            {
+                Console.WriteLine("El número es cero.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
