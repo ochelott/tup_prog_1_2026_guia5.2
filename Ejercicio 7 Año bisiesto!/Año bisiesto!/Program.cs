b5197c6663c546087cf0e76addad474d26cd6bcd
ochelott <benjash8@outlook.com>
2026-05-05 16:16:45 -0300

    Ejercicio 7 año bisiesto

---
+++ Ejercicio 7 Año bisiesto!/Año bisiesto!/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Año_bisiesto_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int año;
+            Console.WriteLine("Ingrese un año: ");
+            año = Convert.ToInt32(Console.ReadLine());
+            if (año % 4 == 0 && año % 100 != 0 || año % 400 == 0)
+            {
+                Console.WriteLine("El año es bisiesto");
+            }
+            else
+            {
+                Console.WriteLine("El año no es bisiesto");
+            }
+            Console.ReadLine();
+        }
+    }
+}
