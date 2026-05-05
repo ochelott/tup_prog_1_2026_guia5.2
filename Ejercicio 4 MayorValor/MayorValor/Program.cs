9a3b3bf9b32e0e42050caf6ebe6eb42fd3205f06
ochelott <benjash8@outlook.com>
2026-05-04 22:48:36 -0300

    Ejercicio 4 MayorValor

---
+++ Ejercicio 4 MayorValor/MayorValor/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayorValor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, mayorValor;
+            Console.WriteLine("Ingrese un número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            mayorValor = num;
+            Console.WriteLine("Ingrese otro número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num > mayorValor)
+            {
+                mayorValor = num;
+            }
+            Console.WriteLine("Ingrese otro número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num > mayorValor)
+            {
+                mayorValor = num;
+            }
+            Console.WriteLine("Ingrese otro número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num > mayorValor)
+            {
+                mayorValor = num;
+            }
+            Console.WriteLine("Ingrese otro número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num > mayorValor)
+            {
+                mayorValor = num;
+            }
+            Console.WriteLine("El mayor valor es: " + mayorValor);
+           
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
